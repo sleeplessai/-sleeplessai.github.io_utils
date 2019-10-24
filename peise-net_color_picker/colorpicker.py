a4b111af991b073eba42bd743b79d1ad0f96bab8
sleeplessai <2018063+sleeplessai@users.noreply.github.com>
2019-10-24 14:45:56 +0800

    fix bugs, optimize page getter, add clean

--- peise-net_color_picker/colorpicker.py
+++ peise-net_color_picker/colorpicker.py
@@ -2,6 +2,7 @@ import requests
 from requests.exceptions import HTTPError
 from lxml import html
 import os
+from time import sleep
 
 # Change DEBUG value to False to collect the whole colors
 DEBUG = True
@@ -10,48 +11,52 @@ DEBUG = True
 kMaxPageNum = 2 if DEBUG else 23
 kBaseUrl = r'http://www.peise.net/color/'
 kHtmlPages = [str(i) + '.html' for i in range(1, kMaxPageNum, 1)]
-kDownloadDir = r'./peise_net/'
+kDownloadDir = r'./color_pages/'
 kCollectionFile = r'collected.html'
 
 def Clean():
-  pass
-  # TODO: remove download dir forcely
-  # if os.path.exists(kDownloadDir):
-  #   os.removedirs(kDownloadDir)
-  # os.mkdir(kDownloadDir)
-  # if os.path.exists(kCollectionFile):
-  #   os.remove(kCollectionFile)
+  print(f'* DEBUG is {DEBUG}.', 'Clean.' if not DEBUG else 'Skip clean.')
+  if DEBUG: return
+  if os.path.exists(kDownloadDir):
+    # directly try to delete all files on dir
+    dl_files = os.listdir(kDownloadDir)
+    for f in dl_files:
+      os.remove(kDownloadDir + f)
+  else:
+    os.mkdir(kDownloadDir)
+  if os.path.exists(kCollectionFile):
+    os.remove(kCollectionFile)
+  print('* Finish clean.')
 
 
-def GetPages(need_to_save=False):
+def GetPages(need_to_save=False, interval_sec=0.2):
+  coll_color_count = 0
+  coll_file_ptr = None
   if not need_to_save:
-    if os.path.exists(kCollectionFile):
-      os.remove(kCollectionFile)
     coll_file_ptr = open(kCollectionFile, 'ab')
-    coll_color_count = 0
-  
   for page in kHtmlPages:
+    response = None
     try: 
       response = requests.get(kBaseUrl + page)
-      print(f'Visiting {page}')
+      print(f'-> Getting {page}')
       response.raise_for_status()
-    except HTTPError as http_err:
-      print(f'HTTPError: {http_err}')
-    except Exception as exceptn:
-      print(f'Exception: {exceptn}')
-    finally:
-      print('Visiting Ok.')
+      print(f'-> Got {page} response.')
       if need_to_save:
-        print(f'Saving {page}')
+        print(f'-> Saving {page}.')
         with open(kDownloadDir + page, 'wb') as f:
           f.write(response.content)
       else:
-        print(f'Collecting {page}')
+        print(f'-> Collecting {page}.')
         coll_color_count += CollectColor(response, coll_file_ptr)
-
-  print(f'Collected-color count: {coll_color_count}')
+    except HTTPError as http_err:
+      print(f'-> HTTPError: {http_err}.')
+    except Exception as exceptn:
+      print(f'-> Exception: {exceptn}.')
+    sleep(interval_sec if interval_sec >= 0.2 else 0.2)
+  print(f'* Collected-color count: {coll_color_count}.')
   response.close()
-  coll_file_ptr.close()
+  if not need_to_save: coll_file_ptr.close()
+  print('* Finish getting pages.')
 
 
 def CollectColor(response, file_ptr):
@@ -61,8 +66,8 @@ def CollectColor(response, file_ptr):
     file_ptr.write(html.tostring(li))
   return len(color_bar_ul_elem)
 
-
 if __name__ == '__main__':
-  if not DEBUG:
-    Clean()
-  GetPages()
+  Clean()
+  GetPages(need_to_save=False)
+  # TODO: The file collected.html after collecting does not show color bars. Fix it.
+  
